dc76e3daabeb483df910aa6d5c0b25304119dac4
remiforest <remi.forest@akilio.com>
2019-01-27 16:51:02 +0100

    updated configure added init dronedata

--- README.md
+++ README.md
@@ -7,11 +7,16 @@ run as root !
 
 
 Prerequisites :
-copy all files in your projet folder on the MapR file system
-run ./setup.sh
+
+- go to your mapr FS root
+- git clone https://github.com/xkr269/e984339581c76ec6117de4ee341ae6f4.git
+- mv e984339581c76ec6117de4ee341ae6f4 teits
+- cd teits
+- ./setup.sh
+- edit init.sh to update the cluster name
+- source init.sh
 
 Configure settings :
-- PROJECT_FOLDER
 - DRONE_MODE :
     - live : use with real drones. Run the pilot.py script on the laptop connected to the drone
     - video : use without drones. Import the videos you want to stream into the data/recording folder as {{zone_name}}.mp4
@@ -23,7 +28,9 @@ python configure.py on the main cluster (maprcli should be available)
 python start.py for main program
 
 Access the zone editor : {{cluster_ip}}/edit
-create the zones for the drones
+create and position the zones for the drones
+At least a home_base zone should exist.
+Typical position is x=0 y=0.
 
 
 In live mode, pilot.py has to run on the computer connected to the drone 
--- configure.py
+++ configure.py
@@ -2,7 +2,7 @@ import os
 import settings
 import time
 import subprocess
-
+from mapr.ojai.storage.ConnectionFactory import ConnectionFactory
 
 print("Starting pre-flight checks ... ")
 
@@ -30,5 +30,21 @@ print("Video stream created")
 create_stream(settings.RECORDING_STREAM)
 print("Recording stream created")
 
+
+print("initializing drones")
+ZONES_TABLE = settings.ZONES_TABLE
+CLUSTER_IP = settings.CLUSTER_IP
+
+# Create database connection
+connection_str = CLUSTER_IP + ":5678?auth=basic;user=mapr;password=mapr;ssl=false"
+connection = ConnectionFactory().get_connection(connection_str=connection_str)
+dronedata_table = connection.get_or_create_store(DRONEDATA_TABLE)
+for DRONE_ID in ["drone_1","drone_2","drone_3"]:
+    dronedata_table.insert_or_replace({"_id":DRONE_ID,
+                                       "flight_data":{"battery":50,"fly_speed":5},
+                                       "log_data":"unset",
+                                       "count":0,
+                                       "connection_status":"disconnected",
+                                       "position": {"zone":"home_base", "status":"landed","offset":0.0}})
 
 print("Configuration complete, run the aplication using start.py")
