cc33dcedf0b96be02e595a3a85fe3c149de62728
remiforest <remi.forest@akilio.com>
2019-01-30 14:51:47 +0100

    fixed pip in setup.sh

--- setup.sh
+++ setup.sh
@@ -6,8 +6,13 @@ bash ./miniconda.sh -b -p /opt/miniconda
 export PATH="/opt/miniconda/bin:$PATH"
 
 
+wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -ivh epel-release-latest-7.noarch.rpm
+
 yum install -y gcc-c++
 yum install -y python-pip
+
+pip install --upgrade pip
 
 pip install maprdb-python-client
 pip install --global-option=build_ext --global-option="--library-dirs=/opt/mapr/lib" --global-option="--include-dirs=/opt/mapr/include/" mapr-streams-python
